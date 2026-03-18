6d412b416a7792a936cda004098b76222d69976c
Mikaiah <mikaiahranaivoson@gmail.com>
2026-03-18 18:20:52 +0300

    update student and welcome http response

--- informatique/java/handling-spring/handling-spring/src/main/java/playsteboy/handling_spring/controllers/WelcomeController.java
+++ informatique/java/handling-spring/handling-spring/src/main/java/playsteboy/handling_spring/controllers/WelcomeController.java
@@ -1,5 +1,6 @@
 package playsteboy.handling_spring.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     @GetMapping("/welcome")
-    public String welcome(@RequestParam String name){
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name){
+        if(name == null || name.isEmpty()){
+            return ResponseEntity.status(400).body("Le nom d'utilisateur n'est pas valide");
+        }
+        return ResponseEntity.status(200).body("Welcome " + name);
     }
 }
